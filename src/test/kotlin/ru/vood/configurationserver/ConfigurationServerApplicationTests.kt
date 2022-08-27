0463e6346fa661bf6f85fac7da70a210b37dba66
vood <balitsky_igor@list.ru>
2022-08-27 14:10:05 +0300

    init

---
+++ src/test/kotlin/ru/vood/configurationserver/ConfigurationServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.vood.configurationserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
