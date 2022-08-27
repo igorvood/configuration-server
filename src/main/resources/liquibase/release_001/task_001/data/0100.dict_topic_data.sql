373ea9db488871913b7a69168c0a0935c1689496
vood <balitsky_igor@list.ru>
2022-08-28 01:52:09 +0300

    init

--- src/main/resources/liquibase/release_001/task_001/data/0100.dict_topic_data.sql
+++ src/main/resources/liquibase/release_001/task_001/data/0100.dict_topic_data.sql
@@ -1,6 +1,8 @@
 insert into DICT_TOPIC(ID, DESCRIPTION, IS_OUR_TOPIC, DEV_NAME, IFT_NAME, PRODUCER_PROP_GRP, CONSUMER_PROP_GRP)
 select 'uasp-mutator-filter-input', 'DESCRIPTION', 1, 'dev-uasp-mutator-filter-input', null, 'DEFAULT_PRODUCER', 'DEFAULT_CONSUMER' from dual union all
+select 'mutator_consumer_2', 'DESCRIPTION', 1, 'dev-uasp-mutator-filter-input', null, 'DEFAULT_PRODUCER', 'DEFAULT_CONSUMER' from dual union all
 select 'ivr__uasp_realtime__bussiness_rules__uaspdto__dlq', 'DESCRIPTION', 1, 'dev_ivr__uasp_realtime__bussiness_rules__uaspdto__dlq', null, 'DEFAULT_PRODUCER', 'DEFAULT_CONSUMER' from dual union all
 select 'dev_ivr__uasp_realtime__business_rules__uaspdto', 'DESCRIPTION', 1, 'dev_ivr__uasp_realtime__business_rules__uaspdto', null, 'DEFAULT_PRODUCER', 'DEFAULT_CONSUMER' from dual union all
+select 'Filter_producer_2', 'DESCRIPTION', 1, 'dev_ivr__uasp_realtime__business_rules__uaspdto', null, 'DEFAULT_PRODUCER', 'DEFAULT_CONSUMER' from dual union all
 select 'ID', 'DESCRIPTION', 1, 'DEV_NAME', null, 'DEFAULT_PRODUCER', 'DEFAULT_CONSUMER' from dual where 1=2
 /
--- src/main/resources/liquibase/release_001/task_001/data/0310.dict_service_profile_topic_data.sql
+++ src/main/resources/liquibase/release_001/task_001/data/0310.dict_service_profile_topic_data.sql
@@ -1,6 +1,8 @@
 insert into dict_service_profile_topic(SERVICE_ID, PROFILE_ID, PRODUCER_TOPIC_ID, PRODUCER_PROP_GRP_REF, CONSUMER_TOPIC_ID, CONSUMER_PROP_GRP_REF)
 select 'Mutator', 'case_65', null, null, 'uasp-mutator-filter-input', 'DEFAULT_CONSUMER' from dual union all
+select 'Mutator', 'case_65', null, null, 'mutator_consumer_2', 'DEFAULT_CONSUMER' from dual union all
 select 'Mutator', 'case_65', 'ivr__uasp_realtime__bussiness_rules__uaspdto__dlq', 'DEFAULT_PRODUCER', null, null from dual UNION ALL
 select 'Mutator', 'case_65', 'dev_ivr__uasp_realtime__business_rules__uaspdto', 'DEFAULT_PRODUCER', null, null from dual  UNION ALL
+select 'Filter', 'case_65', 'Filter_producer_2', 'DEFAULT_PRODUCER', null, null from dual  UNION ALL
 select 'Filter', 'case_65',null, null, 'dev_ivr__uasp_realtime__business_rules__uaspdto', 'DEFAULT_CONSUMER' from dual
 /
