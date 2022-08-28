d7553905a0aa13b39335cf3940be7bc23fdba1ec
vood <balitsky_igor@list.ru>
2022-08-28 18:32:56 +0300

    init

--- src/main/resources/liquibase/release_001/task_001/0100.dict_topic.sql
+++ src/main/resources/liquibase/release_001/task_001/0100.dict_topic.sql
@@ -2,13 +2,30 @@ create table dict_topic
 (
   id varchar2(256) not null,
   constraint dict_act_state_pk primary key (id) using index tablespace t_idx,
+  constraint dict_topic_id_ck check ( instr(id, '-') = 0 ),
     ---
   description varchar2(255) not null,
   is_our_topic number(1) not null,
   constraint dict_topic_is_our_topic_ck check ( is_our_topic in (0, 1) ),
   dev_name varchar2(256) not null,
+  constraint dict_act_state_dev_name_real_uk unique (dev_name) using index tablespace t_idx,
+  ---
   ift_name varchar2(256),
   ift_name_real as (nvl(ift_name, 'ift_'||substr(dev_name,5))),
+  constraint dict_act_state_ift_name_real_uk unique (ift_name_real) using index tablespace t_idx,
+  ---
+  nt_name varchar2(256),
+  nt_name_real as (nvl(ift_name, 'nt_'||substr(dev_name,5))),
+  constraint dict_act_state_nt_name_real_uk unique (nt_name_real) using index tablespace t_idx,
+  ---
+  real_name varchar2(256),
+  real_name_real as (nvl(ift_name, 'rr_'||substr(dev_name,5))),
+  constraint dict_act_state_real_name_real_uk unique (real_name_real) using index tablespace t_idx,
+  ---
+  p0_name varchar2(256),
+  p0_name_real as (nvl(ift_name, 'p0_'||substr(dev_name,5))),
+  constraint dict_act_state_p0_name_real_uk unique (p0_name_real) using index tablespace t_idx,
+  ---
   producer_prop_grp varchar2(64),
   producer_prop_grp_ref as (nvl(producer_prop_grp, 'DEFAULT_PRODUCER')),
   constraint dict_topic_producer_prop_grp_fk FOREIGN KEY (producer_prop_grp_ref)
@@ -43,4 +60,16 @@ comment on column dict_topic.producer_prop_grp_ref is 'группа свойст
 comment on column dict_topic.consumer_prop_grp is 'группа свойств консьюмера, если пусто возьмется по умолчанию'
 /
 comment on column dict_topic.consumer_prop_grp_ref is 'группа свойств консьюмера для констренйнта'
+/
+comment on column dict_topic.nt_name is 'Имя топика на nt стенде.'
+/
+comment on column dict_topic.nt_name_real is 'Имя топика на nt стенде вычисленное.'
+/
+comment on column dict_topic.p0_name is 'Имя топика на prod стенде.'
+/
+comment on column dict_topic.p0_name_real is 'Имя топика на prod стенде вычисленное.'
+/
+comment on column dict_topic.real_name is 'Имя топика на pred prod стенде.'
+/
+comment on column dict_topic.real_name_real is 'Имя топика на pred prod стенде вычисленное.'
 /
--- src/main/resources/liquibase/release_001/task_001/data/0310.dict_service_profile_topic_data.sql
+++ src/main/resources/liquibase/release_001/task_001/data/0310.dict_service_profile_topic_data.sql
@@ -1,5 +1,5 @@
 insert into dict_service_profile_topic(SERVICE_ID, PROFILE_ID, TYPE_TOPIC, TOPIC_ID, TOPIC_GRP_REF)
-select 'Mutator', 'case_65', 'in', 'uasp-mutator-filter-input', 'DEFAULT_CONSUMER' from dual union all
+select 'Mutator', 'case_65', 'in', 'uasp_mutator_filter_input', 'DEFAULT_CONSUMER' from dual union all
 select 'Mutator', 'case_65', 'in', 'mutator_consumer_2', 'DEFAULT_CONSUMER' from dual union all
 select 'Mutator', 'case_65', 'out', 'ivr__uasp_realtime__bussiness_rules__uaspdto__dlq', 'DEFAULT_PRODUCER' from dual UNION ALL
 select 'Mutator', 'case_65', 'out', 'dev_ivr__uasp_realtime__business_rules__uaspdto', 'DEFAULT_PRODUCER' from dual  UNION ALL
