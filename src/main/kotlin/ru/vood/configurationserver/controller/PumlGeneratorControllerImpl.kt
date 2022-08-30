5dd614b6fb85ffccf5bbd05a177b0fc728b62c46
vood <balitsky_igor@list.ru>
2022-08-30 21:18:43 +0300

    init

--- src/main/kotlin/ru/vood/configurationserver/controller/PumlGeneratorControllerImpl.kt
+++ src/main/kotlin/ru/vood/configurationserver/controller/PumlGeneratorControllerImpl.kt
@@ -1,9 +1,6 @@
 package ru.vood.configurationserver.controller
 
 import org.springframework.stereotype.Service
-import ru.vood.configurationserver.controller.dto.Arrow
-import ru.vood.configurationserver.controller.dto.FlinkGraph
-import ru.vood.configurationserver.controller.dto.GraphNode
 import ru.vood.configurationserver.controller.intf.PumlGeneratorControllerIntf
 import ru.vood.configurationserver.repo.intf.PumlGeneratorRepositoryIntf
 
@@ -18,7 +15,6 @@ digraph dfd{
 
     private val tail = """}
 @enduml"""
-
 
     override fun generatePumlByGraphId(graphId: String): String {
         val arrows = pumlGeneratorRepositoryIntf.findByGraphId(graphId)
@@ -41,40 +37,6 @@ digraph dfd{
         return trimMargin
     }
 
-    private /*tailrec*/ fun calcGraphs(
-        arrows: List<Arrow<out GraphNode, out GraphNode>>,
-        flinkGraphs: List<FlinkGraph>
-    ): List<FlinkGraph> {
-        if (arrows.isEmpty()) return flinkGraphs
-
-        val nextArrow = arrows.first()
-
-        val onlyOneGraph = flinkGraphs.withIndex()
-            .filter { indexedGr: IndexedValue<FlinkGraph> ->
-                val gr: FlinkGraph = indexedGr.value
-                gr.arrows
-                    .flatMap { q -> listOf(q.from, q.to) }
-                    .any { n -> n == nextArrow.from || n == nextArrow.to }
-            }
-
-        try {
-            require(onlyOneGraph.isEmpty() || onlyOneGraph.size == 1) { "added arrow must be only ine or none $onlyOneGraph" }
-        } catch (e: Throwable) {
-            e.printStackTrace()
-        }
-
-
-        return if (onlyOneGraph.isEmpty())
-            calcGraphs(arrows.drop(1), flinkGraphs.plus(FlinkGraph(listOf(nextArrow))))
-        else {
-            val currentGraph = onlyOneGraph.first().value
-            val minus = flinkGraphs.minus(currentGraph)
-            val newGraph = currentGraph.copy(arrows = currentGraph.arrows.plus(nextArrow))
-            calcGraphs(arrows.drop(1), minus.plus(newGraph))
-        }
-    }
 }
-
-data class ArrowOld(val from: String, val to: String)
 
 
