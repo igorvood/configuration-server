e4ece0fd1e7a47d556e30c8c0df4b442ea9ed941
vood <balitsky_igor@list.ru>
2022-09-02 19:24:45 +0300

    init

---
+++ src/main/resources/liquibase/release_001/task_001/graph_structure/0106.dict_kafka_property_grp.sql
@@ -0,0 +1,18 @@
+create table dict_kafka_property_grp
+(
+  id varchar2(256) not null,
+  type_read varchar2(256) not null,
+  constraint dict_kafka_property_grp_type_prop_ck check ( type_read in ('prd', 'cns') ),
+  constraint dict_kafka_property_grp_pk primary key (type_read, id) using index tablespace t_idx,
+  description varchar2(512)
+
+)
+/
+comment on table dict_kafka_property_grp is 'Справочник групп настроек продьюсера для кафки.'
+/
+comment on column dict_kafka_property_grp.description is 'Описание.'
+/
+comment on column dict_kafka_property_grp.id is 'Идентификатор группы настроек.'
+/
+comment on column dict_kafka_property_grp.type_read is 'Тип обращения.'
+/
--- src/main/resources/liquibase/release_001/task_001/graph_structure/0310.dict_kafka_prop_value.sql
+++ src/main/resources/liquibase/release_001/task_001/graph_structure/0310.dict_kafka_prop_value.sql
@@ -6,10 +6,7 @@ create table dict_kafka_prop_value
     prop_id varchar2(256) not null,
     constraint dict_kafka_prop_value_pk primary key (type_prop, grp_id, prop_id) using index tablespace t_idx compress 1,
     prop_value varchar2(256) not null,
-    producer_grp_ref as ( case when type_prop='prd' then grp_id end ),
-    constraint dict_kafka_prop_value_producer_ref_fk foreign key (producer_grp_ref) references dict_kafka_prd_property_grp(id),
-    consumer_grp_ref as ( case when type_prop='cns' then grp_id end ),
-    constraint dict_kafka_prop_value_consumer_ref_fk foreign key (consumer_grp_ref) references dict_kafka_cns_property_grp(id),
+    constraint dict_kafka_prop_value_prop_value_ref_fk foreign key (type_prop, grp_id ) references dict_kafka_property_grp(type_read, id),
     constraint dict_kafka_prop_value_prp_fk foreign key (type_prop, prop_id) references dict_kafka_prop (type_prop, prop_id)
 
 )
@@ -23,8 +20,8 @@ comment on column dict_kafka_prop_value.type_prop is 'Тип группы сво
 comment on column dict_kafka_prop_value.prop_id is 'Идентификатор свойства.'
 /
 comment on column dict_kafka_prop_value.prop_value is 'Значение свойства.'
-/
-comment on column dict_kafka_prop_value.producer_grp_ref is 'Идентификатор группы продьюсера.'
-/
-comment on column dict_kafka_prop_value.consumer_grp_ref is 'Идентификатор группы консьюмера.'
+-- /
+-- comment on column dict_kafka_prop_value.producer_grp_ref is 'Идентификатор группы продьюсера.'
+-- /
+-- comment on column dict_kafka_prop_value.consumer_grp_ref is 'Идентификатор группы консьюмера.'
 /
