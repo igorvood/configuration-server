73cdd97a6b13f5acfaccae81cd2923e3e597edc4
vood <balitsky_igor@list.ru>
2022-09-02 22:39:14 +0300

    init

--- src/main/resources/liquibase/release_001/task_001/graph_structure/view/0220.kafka_property_by_sevice.sql
+++ src/main/resources/liquibase/release_001/task_001/graph_structure/view/0220.kafka_property_by_sevice.sql
@@ -6,15 +6,15 @@ with srv as (select TOPIC_ID, DKPV.PROP_ID, DKPV.PROP_VALUE, sn.SERVICE_ID, PROF
              join DICT_KAFKA_PROP_VALUE DKPV on kpg.TYPE_READ = DKPV.TYPE_PROP and kpg.ID = DKPV.GRP_ID
 )
 select
-    --SERVICE_ID||'_'||PROFILE_ID||'.'||
-    lower(srv.TYPE_PROP||'.'||srv.TOPIC_NAME)||'.'||srv.PROP_ID   env_prop_name,
-    srv.TOPIC_ID, srv.PROP_ID, prp.PROPERTY_VAL, srv.SERVICE_ID, srv.PROFILE_ID,  srv.STAND, srv.TYPE_PROP, srv.TOPIC_NAME, srv.GRP_PROP
+    srv.SERVICE_ID, srv.PROFILE_ID,  srv.STAND, srv.PROP_ID, srv.TYPE_PROP,
+    lower(srv.TYPE_PROP||'.'||srv.TOPIC_NAME)||'.'||srv.PROP_ID env_prop_name, prp.PROPERTY_VAL
 from srv
 join KAFKA_PROP_VALUE_by_STAND prp on (prp.GRP_ID, prp.TYPE_PROP, prp.PROP_ID, prp.STAND) = ((srv.GRP_PROP,srv.TYPE_PROP, srv.PROP_ID,  srv.STAND ))
 
-where srv.SERVICE_ID = 'mdm-enrichment' and srv.PROFILE_ID = 'way4'
-and srv.STAND='NOTEBOOK' and srv.TOPIC_ID = 'dev_ivr__uasp_realtime__input_converter__mdm_cross_link__uaspdto'
-order by 1,2
+-- where srv.SERVICE_ID = 'mdm-enrichment' and srv.PROFILE_ID = 'way4'
+-- and srv.STAND='IFT'
+--   and srv.TOPIC_ID = 'dev_ivr__uasp_realtime__input_converter__mdm_cross_link__uaspdto'
+order by SERVICE_ID, PROFILE_ID, STAND, env_prop_name
 /
 comment on table kafka_property_by_sevice is 'вьюха для update нод графов.'
 /
@@ -22,19 +22,13 @@ comment on column kafka_property_by_sevice.SERVICE_ID is 'Идентификат
 /
 comment on column kafka_property_by_sevice.PROFILE_ID is 'Идентификатор сервиса.'
 /
-comment on column kafka_property_by_sevice.topic_id is 'Идентификатор профиля.'
-/
 comment on column kafka_property_by_sevice.STAND is 'Идентификатор профиля.'
-/
-comment on column kafka_property_by_sevice.derrection is 'Идентификатор профиля.'
-/
-comment on column kafka_property_by_sevice.TOPIC_NAME is 'Идентификатор профиля.'
 /
 comment on column kafka_property_by_sevice.ENV_PROP_NAME is 'кей для формирования env файла.'
 /
-comment on column kafka_property_by_sevice.GRP_PROP is 'группа свойств.'
-/
 comment on column kafka_property_by_sevice.PROP_ID is 'идентификатор свойства.'
 /
-comment on column kafka_property_by_sevice.PROP_VALUE is 'Значение свойства.'
+comment on column kafka_property_by_sevice.PROPERTY_VAL is 'идентификатор свойства.'
+/
+comment on column kafka_property_by_sevice.TYPE_PROP is 'идентификатор свойства.'
 /
