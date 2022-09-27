a94c1f68e2f5c57036723be5ecbcd54fe3ff2ac4
vood <balitsky_igor@list.ru>
2022-09-27 08:34:19 +0300

    init

--- src/main/resources/liquibase/release_001/task_001/graph_structure/view/0300.full_flink_service_property_stand.sql
+++ src/main/resources/liquibase/release_001/task_001/graph_structure/view/0300.full_flink_service_property_stand.sql
@@ -2,13 +2,13 @@ create or replace view full_flink_service_property_stand as
 with
     al as
         (
-            select 20 priority, s.service_id, s.profile_id, s.stand, s.prop_id, s.prop_value
+            select 20 priority, s.service_id, s.profile_id, s.stand, s.prop_id, s.prop_value, 'business' type_property
             from flink_property_by_stand s
             union
-            select 100 priority, k.service_id, k.profile_id, k.stand, k.ENV_PROP_NAME, k.PROPERTY_VAL
+            select 100 priority, k.service_id, k.profile_id, k.stand, k.ENV_PROP_NAME, k.PROPERTY_VAL, 'kafka'
             from kafka_property_by_sevice k
             union
-            select 10 priority, q.service_id, q.profile_id, q.stand, q.prop_id, q.prop_value
+            select 10 priority, q.service_id, q.profile_id, q.stand, q.prop_id, q.prop_value, 'topic_name'
             from SERVICE_TOPIC_NAME_BY_STAND q
         )
 select * from al
@@ -19,17 +19,19 @@ select k.service_id, k.profile_id, k.stand, k.prop_id, k.PROPERTY_VAL from kafka
 where STAND = 'DSO' and SERVICE_ID = 'mdm-enrichment' and PROFILE_ID = 'way4'
 /
 
-comment on table full_flink_service_property_stand is 'вьюха для update нод графов.'
+comment on table full_flink_service_property_stand is 'вьюха значения всех свойств сервиса.'
 /
-comment on column full_flink_service_property_stand.priority is 'Идентификатор графа.'
+comment on column full_flink_service_property_stand.priority is 'приоритет для группировки.'
 /
-comment on column full_flink_service_property_stand.SERVICE_ID is 'Идентификатор графа.'
+comment on column full_flink_service_property_stand.SERVICE_ID is 'Идентификатор сервиса.'
 /
-comment on column full_flink_service_property_stand.PROFILE_ID is 'Идентификатор сервиса.'
+comment on column full_flink_service_property_stand.PROFILE_ID is 'Идентификатор профиля.'
 /
-comment on column full_flink_service_property_stand.STAND is 'Идентификатор профиля.'
+comment on column full_flink_service_property_stand.STAND is 'Идентификатор стенда.'
 /
 comment on column full_flink_service_property_stand.PROP_ID is 'идентификатор свойства.'
 /
-comment on column full_flink_service_property_stand.PROP_VALUE is 'идентификатор свойства.'
+comment on column full_flink_service_property_stand.PROP_VALUE is 'значение свойства.'
+/
+comment on column full_flink_service_property_stand.type_property is 'тип свойства.'
 /
