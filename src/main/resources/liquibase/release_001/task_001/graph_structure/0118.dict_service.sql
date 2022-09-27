051b4cc360043ee213241b9034af2bfcfb7e64ae
vood <balitsky_igor@list.ru>
2022-09-27 17:01:00 +0300

    init

--- src/main/resources/liquibase/release_001/task_001/graph_structure/0118.dict_service.sql
+++ src/main/resources/liquibase/release_001/task_001/graph_structure/0118.dict_service.sql
@@ -2,8 +2,8 @@ create table dict_service
 (
   id varchar2(255) not null,
   main_class varchar2(255) not null,
-  constraint dict_service_pk primary key (id) using index tablespace t_idx,
-  constraint dict_service_main_class_uk unique (main_class) using index tablespace t_idx
+  constraint dict_service_pk primary key (id) using index tablespace t_idx
+--   constraint dict_service_main_class_uk unique (main_class) using index tablespace t_idx
 )
 /
 comment on table dict_service is 'Справочник групп настроек для консьюмера топиков.'
--- src/main/resources/liquibase/release_001/task_001/graph_structure/data/profiles/mdm-enrichment/prof-auth.sql
+++ src/main/resources/liquibase/release_001/task_001/graph_structure/data/profiles/mdm-enrichment/prof-auth.sql
@@ -1,5 +1,10 @@
 declare
     l_topics topics;
+    l_graph_id  varchar2(256) := 'very_big';
+    l_SERVICE_ID varchar2(256) := 'mdm_enrichment';
+    l_PROFILE_ID varchar2(256) := 'prof_auth';
+    l_MAIN_CLASS varchar2(256) := 'ru.vtb.uasp.mdm.enrichment.EnrichmentJob';
+
 begin
     with t as (
         SELECT 'out' dirrection , 'dev_rto_batch_ca_customer_card_uaspdto__dlq' topic_name, 'enrichOne.GlobalIdEnrichProperty$.dlqTopic' prop_name from DUAL union
@@ -9,53 +14,15 @@ begin
         SELECT 'in', 'dev_bevents_streaming_input_convertor_profile_auth_uaspdto', 'enrichOne.MainEnrichProperty$.fromTopic' from DUAL union
         SELECT 'out', 'dev_bevents_card_agreement_enrich_out_uaspdto', 'enrichOne.MainEnrichProperty$.toTopicName' from DUAL
     )
-    select * BULK COLLECT INTO l_topics
+    select TOPIC(dirrection, topic_name, prop_name) BULK COLLECT INTO l_topics
     from t;
 
     create_all.create_srv(
-            'very_big111',
-        'enrichment111',
-        'prof_auth111',
-            'ru.vtb.uasp.mdm.enrichment.EnrichmentJob1111',
+            l_graph_id,
+            l_SERVICE_ID,
+            l_PROFILE_ID,
+            l_MAIN_CLASS,
             l_topics
         );
 end;
-/
-    declare
-    graph_id_l  varchar2 := 'very_big';
-begin
-
-    insert into dict_topic_ins(GRAPH_ID, NODE_ID)
-    SELECT 'very_big', 'dev_rto_batch_ca_customer_card_uaspdto__dlq' from DUAL union
-    SELECT 'very_big', 'dev_rto_batch_ca_customer_card_uaspdto' from DUAL union
-    SELECT 'very_big', 'dev_rto_batch_ca_customer_card_uaspdto__status' from DUAL union
-    SELECT 'very_big', 'dev_bevents_card_agreement_enrich_dlq' from DUAL union
-    SELECT 'very_big', 'dev_bevents_streaming_input_convertor_profile_auth_uaspdto' from DUAL union
-    SELECT 'very_big', 'dev_bevents_card_agreement_enrich_out_uaspdto' from DUAL
-
-end;
-
-/
-insert into DICT_SERVICE_INS(GRAPH_ID, SERVICE_ID, PROFILE_ID, MAIN_CLASS)
-SELECT 'very_big', 'mdm-enrichment', 'prof-auth', 'ru.vtb.uasp.mdm.enrichment.EnrichmentJob' from DUAL
-/
-with topic as (
-SELECT 'out' dirrection , 'dev_rto_batch_ca_customer_card_uaspdto__dlq' topic_name, 'enrichOne.GlobalIdEnrichProperty$.dlqTopic' prop_name from DUAL union
-SELECT 'in', 'dev_rto_batch_ca_customer_card_uaspdto', 'enrichOne.GlobalIdEnrichProperty$.fromTopic' from DUAL union
-SELECT 'out', 'dev_rto_batch_ca_customer_card_uaspdto__status', 'enrichOne.GlobalIdEnrichProperty$.toTopicName' from DUAL union
-SELECT 'out', 'dev_bevents_card_agreement_enrich_dlq', 'enrichOne.MainEnrichProperty$.dlqTopic' from DUAL union
-SELECT 'in', 'dev_bevents_streaming_input_convertor_profile_auth_uaspdto', 'enrichOne.MainEnrichProperty$.fromTopic' from DUAL union
-SELECT 'out', 'dev_bevents_card_agreement_enrich_out_uaspdto', 'enrichOne.MainEnrichProperty$.toTopicName' from DUAL
-)
-select 'very_big' GRAPH_ID,
-       case dirrection
-         when 'in' then topic_name
-         when 'out' then 'mdm-enrichment'
-       end BEG_NODE_TYPE,
-       from topic
-
-/
-
-insert into DICT_ARROW(GRAPH_ID, BEG_NODE_TYPE, BEG_NODE_ID, END_NODE_TYPE, END_NODE_ID, PROPERTY_KEY)
-commit
 /
