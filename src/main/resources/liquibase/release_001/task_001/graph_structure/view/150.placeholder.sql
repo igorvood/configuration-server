d6ecf5b955bdeed1c9f7bdae4f0cddf3d4474f60
vood <balitsky_igor@list.ru>
2022-10-04 14:14:43 +0300

    init

---
+++ src/main/resources/liquibase/release_001/task_001/graph_structure/view/150.placeholder.sql
@@ -0,0 +1,13 @@
+create or replace view placeholder as
+with ph as (
+    select SERVICE_ID, PROFILE_ID, STAND, PROP_ID, PROP_VALUE, INSTR(PROP_VALUE, '${') begin_index, INSTR(PROP_VALUE, '}') end_index
+    from FULL_FLINK_SERVICE_PROPERTY_STAND
+)
+select distinct PROP_VALUE from ph
+where ph.begin_index>0 and ph.end_index>0 and ph.begin_index<ph.end_index
+/
+comment on table placeholder is 'Все значения свойств с плейсхолдерами.'
+/
+comment on column full_flink_service_property_stand.priority is 'Значение свойства.'
+/
+
