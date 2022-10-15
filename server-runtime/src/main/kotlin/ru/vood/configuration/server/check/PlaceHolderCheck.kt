414dbabce0c3bfe3d35d95f2006c50d7ae795780
vood <balitsky_igor@list.ru>
2022-10-15 16:43:15 +0300

    init

--- server-runtime/src/main/kotlin/ru/vood/configuration/server/check/PlaceHolderCheck.kt
+++ server-runtime/src/main/kotlin/ru/vood/configuration/server/check/PlaceHolderCheck.kt
@@ -14,7 +14,17 @@ class PlaceHolderCheck(
 
     private val logger = LoggerFactory.getLogger(PlaceHolderCheck::class.java)
     override fun check() {
+        val classAndPlaceholders = holderResolvers.map { it.javaClass to it.placeHolderName }.toMap()
+        val revert = classAndPlaceholders
+            .entries
+            .flatMap { e -> e.value.map { e.key to it } }
+            .groupBy { it.second }
+            .map { it.key to it.value.map { cl -> cl.first }.toSet() }
+            .filter { it.second.size>1 }
+            .map { "Place holder "+it.first+" resolve in several class "+ it.second.map { cl ->  cl.name }
+                .joinToString (", ") }
 
+        require(revert.isEmpty()){revert}
 
 
         val phFun = holderResolvers.flatMap { it.placeHolderName }
