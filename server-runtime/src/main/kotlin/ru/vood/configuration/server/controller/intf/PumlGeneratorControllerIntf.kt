631490cb16b2d0647b75a1c66d85df547391fb76
vood <balitsky_igor@list.ru>
2022-10-24 17:36:08 +0300

    init

--- server-runtime/src/main/kotlin/ru/vood/configuration/server/controller/intf/PumlGeneratorControllerIntf.kt
+++ server-runtime/src/main/kotlin/ru/vood/configuration/server/controller/intf/PumlGeneratorControllerIntf.kt
@@ -2,8 +2,5 @@ package ru.vood.configuration.server.controller.intf
 
 interface PumlGeneratorControllerIntf {
 
-//    @Deprecated("use generatePumlByGraphId")
-//    fun generatePuml(srvId: String, profileId: String): String
-
     fun generatePumlByGraphId(graphId: String): String
 }
