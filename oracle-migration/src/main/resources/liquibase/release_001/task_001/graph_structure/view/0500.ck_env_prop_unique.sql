04cee9c8dde5e69001795f938468122921a0bf17
vood <balitsky_igor@list.ru>
2022-10-26 12:47:28 +0300

    init

--- oracle-migration/src/main/resources/liquibase/release_001/task_001/graph_structure/view/0500.ck_env_prop_unique.sql
+++ oracle-migration/src/main/resources/liquibase/release_001/task_001/graph_structure/view/0500.ck_env_prop_unique.sql
@@ -1,14 +1,14 @@
 create or replace view ck_env_prop_unique as
 with a as (
-    select SERVICE_ID, PROFILE_ID, PROP_ID, count(1)
+    select SERVICE_ID, PROFILE_ID, PROP_ID, STAND, count(1) cnt
     from FULL_FLINK_SERVICE_PROPERTY_STAND
     having  count(1)>1
-    group by SERVICE_ID, PROFILE_ID, PROP_ID
+    group by SERVICE_ID, PROFILE_ID, PROP_ID, STAND
 )
-select a.SERVICE_ID, a.PROFILE_ID, a.PROP_ID, s.TYPE_PROPERTY
+select a.SERVICE_ID, a.PROFILE_ID, a.PROP_ID, s.TYPE_PROPERTY, a.STAND
 from a
-         join FULL_FLINK_SERVICE_PROPERTY_STAND s on (a.SERVICE_ID, a.PROFILE_ID, a.PROP_ID) = ((s.SERVICE_ID, s.PROFILE_ID, s.PROP_ID))
-group by a.SERVICE_ID, a.PROFILE_ID, a.PROP_ID, s.TYPE_PROPERTY
+  join FULL_FLINK_SERVICE_PROPERTY_STAND s on (a.SERVICE_ID, a.PROFILE_ID, a.PROP_ID) = ((s.SERVICE_ID, s.PROFILE_ID, s.PROP_ID))
+group by a.SERVICE_ID, a.PROFILE_ID, a.PROP_ID, s.TYPE_PROPERTY, a.STAND
 /
 comment on table ck_env_prop_unique is 'Проверка уникальности ключей пропертей.'
 /
@@ -20,5 +20,6 @@ comment on column ck_env_prop_unique.PROP_ID is 'Ключ проперти.'
 /
 comment on column ck_env_prop_unique.TYPE_PROPERTY is 'Источник проперти.'
 /
-
+comment on column ck_env_prop_unique.STAND is 'Стенд.'
+/
 
